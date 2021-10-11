fb52daf9dfa88b9b4ac673422de9b5ecf7a5fed6
Ryan Earl <ryanleoearl@gmail.com>
2021-10-11 10:03:32 -0700

    Added some lines from InClass_2

--- project1Server.py
+++ project1Server.py
@@ -1,7 +1,8 @@
-# Import socket module from socket import *
+import socket 
+from socket import *
 
 # In order to terminate the program. Not necessary.
-# import sys
+import sys
 
 # Create a TCP server socket
 # (AF_INET is used for IPv4 protocols)
@@ -10,25 +11,29 @@
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Fill in start
-# Put some code in here
+serverSocket.listen(1)
 # Fill in end
 
 while True:
     print('The server is ready to receive')
     
     # Set up a new connection from the client
-    connectionSocket, addr = #Fill in start.    #Fill in end
+    connectionSocket, addr = serverSocket.accept()  #<---------------
     # If an exception occurs during the execution of try clause
     # the rest of the clause is skipped
     # If the exception type matches the word after except
     # the except clause is executed
     try:
         # Receives the request 
-        message from the clientmessage = #Fill in start.    #Fill in end
+        message from the clientmessage = connectionSocket.recv(2048).decode()  #<---------------
+        print("Received:", message)
+        modifiedMessage = message.upper()
+        connectionSocket.send(modifiedMessage.encode())
+        connectionSocket.close()
         
         # Extract the path of the requested object from the message
         # The path is the second part of HTTP header, identified by [1]
-        filename = message.split()[1]
+        filename = message.split()[1]  
         
         # Because the extracted path of the HTTP request includes
         # a character '\', we read the path from the second character
@@ -36,9 +41,9 @@ while True:
         # print('file opened')
         
         # Store the entire content of the requested file in a temporary buffer
-        outputdata = #Fill in start.    #Fill in end
+        outputdata = f
         
-        # Send the HTTP response header line to the connection socket#Fill in start.    #Fill in end("HTTP/1.1 200 OK\r\n\r\n".encode())
+        # Send the HTTP response header line to the connection socket#Fill in start.    #Fill in end("HTTP/1.1 200 OK\r\n\r\n".encode()) #<---------------
         
         # Send the content of the requested file to the connection socket
         for i in range(0, len(outputdata)):
@@ -52,10 +57,10 @@ while True:
     except IOError:
         print('  *** file not found ***')
         # Send HTTP response message for file not found
-        # #Fill in start.    #Fill in end
+        # #Fill in start.    #Fill in end  #<---------------
             ("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-        #Fill in start.    #Fill in end(
+        #Fill in start.    #Fill in end(   #<---------------
             "<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
         
         # Close the client connection socket
-        #Fill in start.    #Fill in end
+        #Fill in start.    #Fill in end    #<---------------
