e4bd710191d2e5c2654138fb1acdb2f2660cee38
Ryan Earl <ryanleoearl@gmail.com>
2021-10-11 21:12:07 -0700

    Completion of Client

--- Project1Client.py
+++ Project1Client.py
@@ -1,15 +1,15 @@
-# https://docs.python.org/3/library/socket.html
+# https://docs.python.org/3/library/socket.html #Search for 'Echo server program' to find where formatting came from
 from socket import *
-serverName = 'localhost'
-serverPort = 10000
+# b' converts the string into a bytearray
+file_location = b'GET /wireshark-labs/INTRO-wireshark-file1.html HTTP/1.1\r\nHost:gaia.cs.umass.edu\r\n\r\n'
+# construction of the TCP socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-library = "GET /wireshark-labs/INTRO-wireshark-file1.html HTTP/1.1\r\nHost:gaia.cs.umass.edu\r\n\r\n"
-clientSocket.send(library.encode())
 host = "gaia.cs.umass.edu"
 port = 80
+clientSocket.connect((host, port))
+clientSocket.send(file_location)
 while True:
-
     response = clientSocket.recv(1024)
-    print('From Server:\n', response.decode())
-clientSocket.close()
+    if not response:
+        break
+    print(response.decode())
